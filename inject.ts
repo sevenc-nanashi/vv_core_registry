b78f6596623114312e855bcfd296a491a5dc20be
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-05-12 20:15:06 +0900

    Add: ハッシュを追加

--- inject.ts
+++ inject.ts
@@ -1,4 +1,7 @@
 import fs from "fs";
+import https from "https";
+import sha256 from "crypto-js/sha256";
+import { spawn } from "child_process";
 import { Octokit } from "octokit";
 
 (async () => {
@@ -41,32 +44,129 @@ import { Octokit } from "octokit";
   for (const variant of variants) {
     console.log("- " + variant);
   }
-  const variantAssets = new Map<string, [url: string, name: string][]>(
-    [...variants].map((v) => [v, []])
-  );
-  for (const wheel of wheels) {
-    const match = wheel[1].match(/\+(.+?)-/);
-    if (!match) throw new Error("invalid asset name");
-    const variant = match[1];
-    const variantUrls = variantAssets.get(variant);
-    if (!variantUrls) throw new Error("invalid state");
+  const variantAssets = new Map<
+    string,
+    {
+      url: string;
+      name: string;
+      hash: string;
+      python: string;
+      metadata: string;
+      metadataHash: string;
+    }[]
+  >([...variants].map((v) => [v, []]));
+  await fs.promises.mkdir("./cache", { recursive: true });
+  while (wheels.length > 0) {
+    await Promise.all(
+      wheels.splice(0, 5).map(async (wheel) => {
+        const match = wheel[1].match(/\+(.+?)-/);
+        if (!match) throw new Error("invalid asset name");
+        const variant = match[1];
+        const variantUrls = variantAssets.get(variant);
+        if (!variantUrls) throw new Error("invalid state");
 
-    variantUrls.push(wheel);
+        const [url, name] = wheel;
+        let metadataText: string;
+        let hash: string;
+        if (
+          await fs.promises.stat(`./cache/${name}.metadata`).catch(() => false)
+        ) {
+          console.log(`Using cached metadata for ${name}`);
+          metadataText = await fs.promises.readFile(
+            `./cache/${name}.metadata`,
+            "utf-8"
+          );
+          hash = await fs.promises.readFile(`./cache/${name}.sha256`, "utf-8");
+        } else {
+          console.log(`Downloading ${name}`);
+          await new Promise<void>((resolve) => {
+            const handle = (res) => {
+              if (res.headers.location) {
+                console.log(`-> ${res.headers.location}`);
+                https.get(res.headers.location, handle);
+              } else {
+                res.pipe(
+                  fs.createWriteStream(`./cache/${name}`, { flags: "w" })
+                );
+                res.on("end", () => resolve());
+              }
+            };
+            https.get(url, handle);
+          });
+          console.log(`Downloaded ${name}`);
+          hash = await new Promise<string>((resolve) => {
+            const proc = spawn("sha256sum", [`./cache/${name}`]);
+            let stdout = "";
+            proc.stdout.on("data", (data) => {
+              stdout += data;
+            });
+            proc.on("close", () => {
+              const hash = stdout.split(" ")[0];
+              if (!hash) throw new Error("invalid hash");
+              resolve(hash);
+            });
+          });
+          await fs.promises.writeFile(`./cache/${name}.sha256`, hash, "utf-8");
+          metadataText = await new Promise<string>((resolve) => {
+            const strippedName = name.match(/^([^-]+-[^-]+)-.+\.whl$/)?.[1];
+            if (!strippedName) throw new Error("invalid wheel");
+
+            const metadataPath = `${strippedName}.dist-info/METADATA`;
+            const proc = spawn("unzip", [
+              "-p",
+              `./cache/${name}`,
+              metadataPath,
+            ]);
+            let stdout = "";
+            proc.stdout.on("data", (data) => {
+              stdout += data;
+            });
+
+            proc.on("close", () => {
+              resolve(stdout);
+            });
+          });
+          await fs.promises.writeFile(
+            `./cache/${name}.metadata`,
+            metadataText,
+            "utf-8"
+          );
+          await fs.promises.unlink(`./cache/${name}`);
+          console.log(`Saved metadata for ${name}`);
+        }
+        const metadataLines = metadataText.split("\n");
+        const requiresPythonLine = metadataLines.find((line) =>
+          line.startsWith("Requires-Python:")
+        );
+        if (!requiresPythonLine) throw new Error("invalid wheel");
+        const requiresPython = requiresPythonLine.split(":")[1].trim();
+        const metadataHash = sha256(metadataText).toString();
+        variantUrls.push({
+          url,
+          name,
+          hash,
+          python: requiresPython,
+          metadata: metadataText,
+          metadataHash: metadataHash,
+        });
+      })
+    );
   }
 
   const cpuAssets = variantAssets.get("cpu");
   if (!cpuAssets) throw new Error("invalid state");
   variantAssets.set(".", cpuAssets);
 
-  for (const [variant, urls] of variantAssets) {
+  for (const [variant, wheels] of variantAssets) {
     await fs.promises.mkdir(`./dist/${variant}/voicevox-core`, {
       recursive: true,
     });
     await fs.promises.writeFile(
       `./dist/${variant}/voicevox-core/index.html`,
-      `<!DOCTYPE html><html><body>${urls
+      `<!DOCTYPE html><html><body>${wheels
         .map(
-          ([url, name]) => `<a href="${url}">${name.replace(/\+.+?-/, "")}</a>`
+          ({ url, name, python, hash }) =>
+            `<a href="${url}#sha256=${hash}" data-requires-python="${python}">${name}</a>`
         )
         .join("<br>")}</body></html>`
     );
@@ -75,7 +175,7 @@ import { Octokit } from "octokit";
     } else {
       await fs.promises.writeFile(
         `./dist/${variant}/index.html`,
-        `<a href="./voicevox-core/" data-requires-python=">=3.8.0">voicevox-core</a>`
+        `<a href="./voicevox-core/">voicevox-core</a>`
       );
       console.log(`Wrote package list for ${variant}`);
     }
--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import Prism from "@uiw/react-prismjs";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import exampleTts from "./examples/tts.py?raw";
 import exampleRequirements from "./examples/requirements.txt?raw";
+import examplePoetry from "./examples/poetry.toml?raw";
 import "prismjs/components/prism-python";
 import "prismjs/components/prism-toml";
 import "prismjs/components/prism-bash";
 import "react-tabs/style/react-tabs.css";
+
+const version = "0.14.3";
+
 function App() {
   return (
     <>
@@ -26,22 +30,28 @@ function App() {
           <TabPanel>
             <Prism
               language="bash"
-              source={`pip install --extra-index-url https://voicevox.github.io/voicevox_core/ voicevox_core==0.15.0+cpu`}
+              source={`pip install --extra-index-url https://voicevox.github.io/voicevox_core/ voicevox_core==${version}`}
             />
           </TabPanel>
           <TabPanel>
             <Prism
               language="txt"
-              source={exampleRequirements}
+              source={exampleRequirements.replace("!{version}", version)}
             />
           </TabPanel>
           <TabPanel>
-            <Prism language="python" source={exampleTts} />
+            <Prism
+              language="toml"
+              source={examplePoetry.replace("!{version}", version)}
+            />
           </TabPanel>
         </Tabs>
-        <div className="text-center bg-primary text-white text-xl rounded mx-auto px-8 py-4 shadow-sm">
+        <a
+          href="https://voicevox.github.io/voicevox_core/apis/python_api/"
+          className="block text-center bg-primary text-white text-xl rounded mx-auto px-8 py-4 shadow-sm hover:shadow-md focus:shadow-md transition-shadow duration-300"
+        >
           ドキュメントをチェック
-        </div>
+        </a>
       </main>
     </>
   );
