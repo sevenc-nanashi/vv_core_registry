3cee61da6eb51ed281674d4779bf631220fb9550
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-09-18 22:14:29 +0900

    Add: Java APIのinjectを追加

--- inject.ts
+++ inject.ts
@@ -3,22 +3,38 @@ import https from "https";
 import sha256 from "crypto-js/sha256";
 import { spawn } from "child_process";
 import { Octokit } from "octokit";
+import { Extract } from "unzipper";
+import xml from "xml-js";
+import { Endpoints } from "@octokit/types";
 
-(async () => {
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-  const releases = await octokit.rest.repos.listReleases({
-    owner: "voicevox",
-    repo: "voicevox_core",
+type Release =
+  Endpoints["GET /repos/{owner}/{repo}/releases"]["response"]["data"][0];
+const javaVariants = [
+  "voicevoxcore",
+  "voicevoxcore-cuda",
+  "voicevoxcore-directml",
+  "voicevoxcore-android",
+];
+
+const download = async (url: string, path: string) => {
+  await new Promise<void>((resolve) => {
+    const handle = (res) => {
+      if (res.headers.location) {
+        console.log(`-> ${res.headers.location}`);
+        https.get(res.headers.location, handle);
+      } else {
+        res.pipe(fs.createWriteStream(path));
+        res.on("end", () => resolve());
+      }
+    };
+    https.get(url, handle);
   });
+};
 
-  const releasesToList = releases.data.filter(
-    (release) =>
-      !release.draft &&
-      release.assets.find((asset) => asset.name.endsWith(".whl"))
-  );
+const getPythonPackages = async (releases: Release[]) => {
   const wheels: [url: string, name: string][] = [];
 
-  for (const release of releasesToList) {
+  for (const release of releases) {
     for (const asset of release.assets) {
       if (asset.name.endsWith(".whl")) {
         wheels.push([asset.browser_download_url, asset.name]);
@@ -29,7 +45,7 @@ import { Octokit } from "octokit";
   console.log("Found wheels:", wheels.length);
 
   const variants = new Set(
-    releasesToList.flatMap((release) =>
+    releases.flatMap((release) =>
       release.assets
         .filter((asset) => asset.name.endsWith(".whl"))
         .map((asset) => {
@@ -78,20 +94,7 @@ import { Octokit } from "octokit";
           hash = await fs.promises.readFile(`./cache/${name}.sha256`, "utf-8");
         } else {
           console.log(`Downloading ${name}`);
-          await new Promise<void>((resolve) => {
-            const handle = (res) => {
-              if (res.headers.location) {
-                console.log(`-> ${res.headers.location}`);
-                https.get(res.headers.location, handle);
-              } else {
-                res.pipe(
-                  fs.createWriteStream(`./cache/${name}`, { flags: "w" })
-                );
-                res.on("end", () => resolve());
-              }
-            };
-            https.get(url, handle);
-          });
+          await download(url, `./cache/${name}`);
           console.log(`Downloaded ${name}`);
           hash = await new Promise<string>((resolve) => {
             const proc = spawn("sha256sum", [`./cache/${name}`]);
@@ -179,4 +182,153 @@ import { Octokit } from "octokit";
       console.log(`Wrote package list for ${variant}`);
     }
   }
+};
+
+const getJavaPackages = async (releases: Release[]) => {
+  const archives: { url: string; version: string }[] = [];
+
+  for (const release of releases) {
+    for (const asset of release.assets) {
+      if (asset.name === "java_packages.zip") {
+        archives.push({
+          url: asset.browser_download_url,
+          version: release.tag_name,
+        });
+      }
+    }
+  }
+
+  console.log("Found archives: ", archives.length);
+
+  await Promise.all(
+    archives.map(async ({ url, version }) => {
+      if (
+        await fs.promises
+          .stat(`./cache/java_packages-${version}`)
+          .catch(() => false)
+      ) {
+        console.log(`Using cached archive for ${version}`);
+        return;
+      }
+
+      if (
+        await fs.promises
+          .stat(`./cache/java_packages-${version}.zip`)
+          .catch(() => false)
+      ) {
+        console.log(`Using cached archive for ${version}`);
+      } else {
+        console.log(`Downloading archive for ${version}`);
+        await download(url, `./cache/java_packages-${version}.zip`);
+
+        console.log(`Downloaded archive for ${version}`);
+      }
+
+      console.log(`Extracting archive for ${version}`);
+      await new Promise<void>((resolve) => {
+        fs.createReadStream(`./cache/java_packages-${version}.zip`)
+          .pipe(
+            Extract({
+              path: `./cache/java_packages-${version}`,
+            })
+          )
+          .on("close", () => {
+            resolve();
+          });
+      });
+      console.log(`Extracted archive for ${version}`);
+    })
+  );
+
+  for (const name of javaVariants) {
+    await fs.promises.mkdir(`./dist/jp/hiroshiba/voicevoxcore/${name}`, {
+      recursive: true,
+    });
+  }
+
+  for (const name of await fs.promises.readdir("./cache")) {
+    if (!name.startsWith("java_packages-") || name.endsWith(".zip")) continue;
+
+    for (const dir of await fs.promises.readdir(
+      `./cache/${name}/jp/hiroshiba/voicevoxcore`
+    )) {
+      const dirs = await fs.promises.readdir(
+        `./cache/${name}/jp/hiroshiba/voicevoxcore/${dir}`
+      );
+      for (const version of dirs) {
+        if (version.endsWith(".xml")) continue;
+        await fs.promises.cp(
+          `./cache/${name}/jp/hiroshiba/voicevoxcore/${dir}/${version}`,
+          `./dist/jp/hiroshiba/voicevoxcore/${dir}/${version}`,
+          {
+            recursive: true,
+          }
+        );
+      }
+    }
+  }
+  for (const name of javaVariants) {
+    await fs.promises.writeFile(
+      `./dist/jp/hiroshiba/voicevoxcore/${name}/maven-metadata.xml`,
+      xml.js2xml(
+        {
+          _declaration: {
+            _attributes: {
+              version: "1.0",
+              encoding: "utf-8",
+            },
+          },
+          metadata: {
+            groupId: "jp.hiroshiba.voicevoxcore",
+            artifactId: name,
+            versioning: {
+              latest: archives[0].version,
+              release: archives[0].version,
+              versions: {
+                version: archives.map((archive) => archive.version),
+              },
+            },
+          },
+        },
+        {
+          compact: true,
+        }
+      )
+    );
+  }
+
+  await fs.promises.writeFile(
+    `./dist/archetype-catalog.xml`,
+    xml.js2xml({
+      _declaration: {
+        _attributes: {
+          version: "1.0",
+          encoding: "utf-8",
+        },
+      },
+      "archetype-catalog": {
+        archetypes: {
+          archetype: javaVariants.map((name) => ({
+            groupId: "jp.hiroshiba.voicevoxcore",
+            artifactId: name,
+            version: archives[0].version,
+            description: name,
+          })),
+        },
+      },
+    })
+  );
+};
+
+(async () => {
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+  const releases = await octokit.rest.repos
+    .listReleases({
+      owner: "sevenc-nanashi",
+      repo: "voicevox_core",
+    })
+    .then((res) => res.data.filter((release) => !release.draft));
+  await fs.promises.mkdir("./dist", { recursive: true });
+  await getPythonPackages(releases);
+  await getJavaPackages(releases);
 })();
